00faf62ffe11131aab0ea845b8a40ed9cbe89813
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-09-07 10:46:47 +0100

    json file created

---
+++ src/movies.js
@@ -0,0 +1,286 @@
+export const movies = [
+  {
+    "title": "The Grudge",
+    "year": 2020,
+    "cast": [
+      "Andrea Riseborough",
+      "Demián Bichir",
+      "John Cho",
+      "Betty Gilpin",
+      "Lin Shaye",
+      "Jacki Weaver"
+    ],
+    "genres": [
+      "Horror",
+      "Supernatural"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/3/34/The_Grudge_2020_Poster.jpeg",
+    "thumbnail_width": 220,
+    "thumbnail_height": 326
+  },
+
+  {
+    "title": "Underwater",
+    "year": 2020,
+    "cast": [
+      "Kristen Stewart",
+      "Vincent Cassel",
+      "Jessica Henwick",
+      "John Gallagher Jr.",
+      "Mamoudou Athie",
+      "T.J. Miller"
+    ],
+    "genres": [
+      "Action",
+      "Horror",
+      "Science Fiction"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/4/4a/Underwater_poster.jpeg",
+    "thumbnail_width": 250,
+    "thumbnail_height": 398
+  },
+
+  {
+    "title": "Like a Boss",
+    "year": 2020,
+    "cast": [
+      "Tiffany Haddish",
+      "Rose Byrne",
+      "Salma Hayek",
+      "Jennifer Coolidge",
+      "Billy Porter"
+    ],
+    "genres": [
+      "Comedy"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/9/9a/LikeaBossPoster.jpg",
+    "thumbnail_width": 259,
+    "thumbnail_height": 383
+  },
+
+  {
+    "title": "Three Christs",
+    "year": 2020,
+    "cast": [
+      "Richard Gere",
+      "Peter Dinklage",
+      "Walton Goggins",
+      "Bradley Whitford"
+    ],
+    "genres": [
+      "Drama"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/a/a1/Three_Christs_poster.jpg",
+    "thumbnail_width": 259,
+    "thumbnail_height": 383
+  },
+
+  {
+    "title": "Inherit the Viper",
+    "year": 2020,
+    "cast": [
+      "Josh Hartnett",
+      "Margarita Levieva",
+      "Chandler Riggs",
+      "Bruce Dern",
+      "Owen Teague"
+    ],
+    "genres": [
+      "Crime",
+      "Drama"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/1/1c/Inherit_the_Viper_%282019%29_Film_Poster.jpg",
+    "thumbnail_width": 236,
+    "thumbnail_height": 350
+  },
+
+  {
+    "title": "The Sonata",
+    "year": 2020,
+    "cast": [
+      "Freya Tingley",
+      "Simon Abkarian",
+      "Rutger Hauer",
+      "James Faulkner"
+    ],
+    "genres": [
+      "Mystery",
+      "Thriller"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/1/13/The_Sonata_%282018%29_Film_Poster.jpg",
+    "thumbnail_width": 246,
+    "thumbnail_height": 350
+  },
+
+  {
+    "title": "The Murder of Nicole Brown Simpson",
+    "year": 2020,
+    "cast": [
+      "Mena Suvari",
+      "Nick Stahl",
+      "Taryn Manning"
+    ],
+    "genres": [
+      "Crime",
+      "Horror"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/e/ed/The_Murder_of_Nicole_Brown_Simpson_poster.jpg",
+    "thumbnail_width": 263,
+    "thumbnail_height": 380
+  },
+
+  {
+    "title": "Bad Boys for Life",
+    "year": 2020,
+    "cast": [
+      "Will Smith",
+      "Martin Lawrence",
+      "Vanessa Hudgens",
+      "Alexander Ludwig",
+      "Charles Melton",
+      "Paola Núñez",
+      "Kate del Castillo",
+      "Nicky Jam",
+      "Joe Pantoliano"
+    ],
+    "genres": [
+      "Action",
+      "Comedy"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/9/90/Bad_Boys_for_Life_poster.jpg",
+    "thumbnail_width": 219,
+    "thumbnail_height": 325
+  },
+
+  {
+    "title": "Dolittle",
+    "year": 2020,
+    "cast": [
+      "Robert Downey Jr.",
+      "Antonio Banderas",
+      "Michael Sheen",
+      "Emma Thompson",
+      "Rami Malek",
+      "John Cena",
+      "Kumail Nanjiani",
+      "Octavia Spencer",
+      "Tom Holland",
+      "Craig Robinson",
+      "Ralph Fiennes",
+      "Selena Gomez",
+      "Marion Cotillard"
+    ],
+    "genres": [
+      "Adventure",
+      "Fantasy"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/1/1f/Dolittle_%282020_film_poster%29.png",
+    "thumbnail_width": 220,
+    "thumbnail_height": 326
+  },
+
+  {
+    "title": "A Fall from Grace",
+    "year": 2020,
+    "cast": [
+      "Crystal R. Fox",
+      "Phylicia Rashad",
+      "Bresha Webb",
+      "Mehcad Brooks",
+      "Cicely Tyson",
+      "Tyler Perry"
+    ],
+    "genres": [
+      "Thriller"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/4/4e/AFallFromGrace.png",
+    "thumbnail_width": 259,
+    "thumbnail_height": 383
+  },
+
+  {
+    "title": "The Gentlemen",
+    "year": 2020,
+    "cast": [
+      "Matthew McConaughey",
+      "Charlie Hunnam",
+      "Henry Golding",
+      "Michelle Dockery",
+      "Jeremy Strong",
+      "Eddie Marsan",
+      "Colin Farrell",
+      "Hugh Grant"
+    ],
+    "genres": [
+      "Action",
+      "Comedy"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/0/06/The_Gentlemen_poster.jpg",
+    "thumbnail_width": 259,
+    "thumbnail_height": 383
+  },
+
+  {
+    "title": "The Turning",
+    "year": 2020,
+    "cast": [
+      "Mackenzie Davis",
+      "Finn Wolfhard",
+      "Brooklynn Prince",
+      "Joely Richardson"
+    ],
+    "genres": [
+      "Horror",
+      "Supernatural"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/a/a2/The_Turning_poster_2020.jpg",
+    "thumbnail_width": 251,
+    "thumbnail_height": 397
+  },
+
+  {
+    "title": "The Last Full Measure",
+    "year": 2020,
+    "cast": [
+      "Sebastian Stan",
+      "Christopher Plummer",
+      "William Hurt",
+      "Ed Harris",
+      "Samuel L. Jackson",
+      "Peter Fonda",
+      "LisaGay Hamilton",
+      "Jeremy Irvine",
+      "Diane Ladd",
+      "Amy Madigan",
+      "John Savage",
+      "Bradley Whitford"
+    ],
+    "genres": [
+      "Drama",
+      "War"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/9/9d/The_Last_Full_Measure_2019_poster.jpg",
+    "thumbnail_width": 256,
+    "thumbnail_height": 378
+  },
+
+  {
+    "title": "John Henry",
+    "year": 2020,
+    "cast": [
+      "Terry Crews",
+      "Ludacris",
+      "Jamila Velazquez",
+      "Ken Foree",
+      "Tyler Alvarez",
+      "Joseph Julian Soria"
+    ],
+    "genres": [
+      "Drama",
+      "Thriller"
+    ],
+    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/b/b8/JohnHenryPoster.jpeg",
+    "thumbnail_width": 220,
+    "thumbnail_height": 316
+  },
+]
