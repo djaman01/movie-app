56ed76e0182d2eda8583b232aba50536ab90ae2b
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-09-10 21:50:13 +0100

    ajout remove button et onClick property

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
+
 export default function Button() {
 
+const [showDiv, setShowDiv] = useState (false);
+
+const handleAdd = () => setShowDiv(true);
+
+const handleRemove= () => setShowDiv(false);
+
+
 return (
-  <button> Add </button>
+  <>
+  <button onClick={handleAdd}> Add </button>
+
+  <button onClick = {handleRemove}> Remove </button>
+
+  <div>
+    {(showDiv==true) && <p>Movie added to favorite</p>}
+  </div>
+  </>
 );
 }
